3e2d44ca3adf30908f19f522c6a3c33e9fb909b7
Olti Berisha <olti.berisha.150@gmail.com>
2025-05-13 00:00:57 +0200

    update landing page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { AuthProvider } from "./context/AuthContext";
 import LoginForm from "./components/Auth/LoginForm";
 import RegisterForm from "./components/Auth/RegisterForm";
 import CvAnalysisPage from "./components/CvAnalysis/CvAnalysisPage";
+import LandingPage from "./components/LandingPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 import "./App.css";
@@ -17,9 +18,12 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/resume" replace />} />
+          {/* Public Routes */}
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
+          
+          {/* Protected Routes */}
           <Route
             path="/resume"
             element={
@@ -28,7 +32,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="*" element={<Navigate to="/resume" replace />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </AuthProvider>
